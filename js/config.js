58d9f1048a15296e73a57005eb935730d39dc64e
Athoillah Almadany <athoillahalma836@gmail.com>
2026-05-23 08:40:56 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -4,6 +4,33 @@ export const MQTT_BROKER = "wss://broker.hivemq.com:8884/mqtt";
 export const MQTT_TOPIC = "smartblind/status";
 
 export const STATUS = {
+kiri_depan: {
+  message: "AWAS KIRI DAN DEPAN ADA HALANGAN",
+  speech: "Awas kiri dan depan ada halangan",
+  type: "danger",
+  icon: "left",
+},
+
+kanan_depan: {
+  message: "AWAS DEPAN DAN KANAN ADA HALANGAN",
+  speech: "Awas depan dan kanan ada halangan",
+  type: "danger",
+  icon: "right",
+},
+
+kiri_kanan: {
+  message: "AWAS KIRI DAN KANAN ADA HALANGAN",
+  speech: "Awas kiri dan kanan ada halangan",
+  type: "danger",
+  icon: "front",
+},
+
+bahaya_total: {
+  message: "AWAS BANYAK HALANGAN DI SEKITAR",
+  speech: "Awas banyak halangan di sekitar",
+  type: "danger",
+  icon: "front",
+},
   kiri: {
     message: "AWAS KIRI ADA HALANGAN",
     speech: "Awas kiri ada halangan",
